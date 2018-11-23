04a576da1cbd8b3f035bd6d393d88a0330742c59
sansna <1185280650@qq.com>
2018-11-23 22:12:15 +0800

    adds several files

--- a.go
+++ a.go
@@ -1,7 +1,9 @@
 package main
+
 import "git.ixiaochuan.cn/xclib/common/utils"
 import "time"
 import "fmt"
+
 func main() {
 	fmt.Println(utils.TimestampToTimeString(time.Now().Unix()))
 	fmt.Println(len("asdf"))
