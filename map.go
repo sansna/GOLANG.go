48e4986df5b3ba7374241fee6ee1fdd352e08d3e
sansna <1185280650@qq.com>
2018-11-25 23:52:44 +0800

    adds map example

---
+++ map.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+	m := make(map[string]interface{})
+	m["life"]=1
+	m["is"]=2
+	ebd := make(map[string]string)
+	m["good"]=ebd
+	ebd["inner1"]="golang"
+	ebd["inner2"]="again"
+	fmt.Println(m)
+}
