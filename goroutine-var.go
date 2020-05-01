d630628549ed5b6babf712c2fdb56805f6457905
sansna <1185280650@qq.com>
2020-05-01 23:14:01 +0800

    @Wentao[master]: udpate  - sansna

--- goroutine-var.go
+++ goroutine-var.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
+
+func testlife() {
+	// lifetime
+	z := 1
+	// z'val is preserved after testlifes's exit, even after GC-ed
+	go func() {
+		for true {
+			time.Sleep(time.Millisecond * 20)
+			fmt.Println(z)
+		}
+	}()
+	time.Sleep(time.Second * 3)
+	// run gc
+	runtime.GC()
+	return
+}
 
 func main() {
 	// This does not work.
@@ -20,4 +37,12 @@ func main() {
 	fmt.Println("first:", b)
 	time.Sleep(time.Second * 2)
 	fmt.Println("second:", b)
+
+	fmt.Println("go into testlife")
+	testlife()
+	fmt.Println("out of testlife")
+	runtime.GC()
+
+	time.Sleep(time.Second * 10)
+	return
 }
