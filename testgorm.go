d65cc66eeffe4f548c73b33669e23e6c90fc711a
sansna <1185280650@qq.com>
2020-11-03 21:43:05 +0800

    @Wentao[master]: uip - sansna

---
+++ testgorm.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"git.ixiaochuan.cn/pp_server/live/service/common/cfgst"
+	"git.ixiaochuan.cn/pp_server/live/service/common/cmodel"
+)
+
+type Account struct {
+	Id int64 `json:"id,omitempty" gorm:"column:id"`
+	Et int64 `json:"et,omitempty" gorm:"column:et"`
+}
+
+func (Account) TableName() string {
+	return "json"
+}
+
+func main() {
+	cmodel.InitMysqlGORM(cfgst.MySql{
+		Hostport: "127.0.0.1:3306",
+		User:     "root",
+		Password: "zz",
+		DbName:   "account",
+	}, true)
+
+	session := cmodel.DBNew()
+	// this does set et = 100
+	session.Model(Account{}).Where("id = ?", 2).Select("et").Updates(&Account{
+		Et: 100,
+	})
+	// this does not set et = 0, gorm ignores blank fields
+	session.Model(Account{}).Where("id = ?", 2).Select("et").Updates(&Account{
+		Et: 0,
+	})
+}
